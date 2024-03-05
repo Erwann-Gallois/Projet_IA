a27e419b089d77e90366cec2cc436b067a2fd4b5
Erwann_Gallois <gallois.erwann@gmail.com>
2024-03-05 17:36:13 +0100

    Mise à jour du code

---
+++ city.py
@@ -0,0 +1,20 @@
+import numpy as np
+class City :
+    # * Constructeur de la classe "city"
+    def __init__ (self, name, x, y):
+        self.name = name
+        self.x = x
+        self.y = y
+    
+    def distance (self, city):
+        # * Distance entre deux villes en x
+        xDis = abs(self.x - city.x)
+        # * Distance entre deux villes en y
+        yDis = abs(self.y - city.y)
+        # * Distance entre deux villes en utilisant le théorème de Pythagore
+        distance = np.sqrt((xDis ** 2) + (yDis ** 2))
+        return distance
+    
+    def toString (self):
+        return self.name + " (" + str(self.x) + ", " + str(self.y) + ")"
+    
--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+import numpy as np
+nbre_ville = 5
+nbre_chemin = 10
+nbre_generation = 100
 
+cities = []
+for i in range(nbre_ville):
+    city = City("Ville " + str(i), np.random.randint(0, 200), np.random.randint(0, 200))
