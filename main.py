1f76fca538219b94f566e0b3795b4a0a399a0aa5
Erwann_Gallois <gallois.erwann@gmail.com>
2024-03-07 16:19:07 +0100

    Ajouter des chemins aléatoires entre les villes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random as rd
 import matplotlib.pyplot as plt
 from city import City
 from chemin import Chemin
@@ -6,48 +7,33 @@ from chemin import Chemin
 taille_x = 500
 taille_y = 500
 nbre_ville = 5
+nbre_parents = 2
 nbre_generation = 100
 score_seuil = 50
 mutation_rate = 0.01
 cities = []
 # Creer des villes aleatoires
 for i in range(nbre_ville):
-    city = City("Ville " + str(i+1), np.random.randint(0, taille_x), np.random.randint(0, taille_y))
+    city = City("Ville " + str(i+1), rd.randrange(0, taille_x), rd.randrange(0, taille_y))
     cities.append(city)
 
-# Creer des chemins aleatoires
-chemins = []
-for i in range(nbre_ville - 1):
-    chemin = Chemin(cities[i], cities[i+1])
-    chemins.append(chemin)
+# Creation des chemins (Toutes les villes sont connectées)
+# chemins = []
+# for i in range(nbre_ville - 1):
+#     for j in range (i, len(cities)):
+#         if (cities[i] != cities[j]):
+#             chemins.append(Chemin(cities[i], cities[j]))
 
-# Assurez-vous que chaque ville a au moins un chemin en créant un chemin supplémentaire entre la dernière ville et la première ville
-chemin = Chemin(cities[-1], cities[0])
-chemins.append(chemin)
+# Generations des parents
+parents = []
+for i in range (0, nbre_parents):
+    rd.shuffle(cities)
+    for j in range(0, (len(cities)-1)):
+        parents[i] = (Chemin(cities[j], cities[j+1]))
 
-# Creer des chemins aleatoires entre chaque ville sans doublons
-for i in range(nbre_ville - 1):
-    depart = cities[np.random.randint(0, nbre_ville)]
-    arrivee = cities[np.random.randint(0, nbre_ville)]
-    if depart == arrivee:
-        while depart == arrivee:
-            arrivee = cities[np.random.randint(0, nbre_ville)]
-    for chemin in chemins : 
-        if (depart == chemin.depart and arrivee == chemin.arrivee) or (depart == chemin.arrivee and arrivee == chemin.depart):
-            while (depart == chemin.depart and arrivee == chemin.arrivee) or (depart == chemin.arrivee and arrivee == chemin.depart):
-                depart = cities[np.random.randint(0, nbre_ville)]
-                arrivee = cities[np.random.randint(0, nbre_ville)]
-                if depart == arrivee:
-                    while depart == arrivee:
-                        arrivee = cities[np.random.randint(0, nbre_ville)]
-    chemin = Chemin(depart, arrivee)
-    chemins.append(chemin) 
-
-for chemin in chemins:
-    print(chemin.toString())
-
-for i in range(nbre_ville - 1):
-    print(chemins[i].toString())
+for parent in parents:
+    print (parent.toString())
+        
 
 # Afficher les villes sur une grille
 fig, ax = plt.subplots()
@@ -57,10 +43,10 @@ for city in cities:
     ax.text(city.x, city.y, city.name) # Affiche le nom de la ville
 
 # Creer un chemin entre chaque ville
-for chemin in chemins:
-    ax.plot([chemin.depart.x, chemin.arrivee.x], [chemin.depart.y, chemin.arrivee.y], color=np.random.rand(3,), label = str(chemin.score))
+# for chemin in chemins:
+#     ax.plot([chemin.depart.x, chemin.arrivee.x], [chemin.depart.y, chemin.arrivee.y], color=np.random.rand(3,), label = "{:.2f}".format(chemin.score))
 
-ax.legend(loc='upper right') # Affiche la légende en haut à droite
+ax.legend(loc='best') # Affiche la légende en haut à droite
 ax.set_xticks([])  # Crée une grille x avec des intervalles de 20
 ax.set_yticks([])  # Crée une grille y avec des intervalles de 20
 ax.axis('off')
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+for chemin in chemins:
+#     ax.plot([chemin.depart.x, chemin.arrivee.x], [chemin.depart.y, chemin.arrivee.y], color=np.random.rand(3,), label = "{:.2f}".format(chemin.score))
