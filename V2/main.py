15dc77f932b1943f88e462adbb4fc7cc5822a0ad
Erwann-Gallois <gallois.erwann@gmail.com>
2024-03-26 20:34:47 +0100

    Ajout de la fonction d'ajout de ville aléatoire et de la fonction de mutation

--- V2/main.py
+++ V2/main.py
@@ -7,8 +7,6 @@ import numpy as np
 # --------------------------- DataFrame des villes --------------------------- #
 ville_df = pd.DataFrame(columns=["Nom", "x", "y"])
 
-# --------------------------- DataFrame des chemins -------------------------- #
-
 # ---------------------------------------------------------------------------- #
 #                                  Constantes                                  #
 # ---------------------------------------------------------------------------- #
@@ -22,11 +20,19 @@ TAILLE_GRILLE_X = 200
 TAILLE_GRILLE_Y = 200
 NBRE_VILLE = 10
 
-# ----------------------- Creation ville aléatoirement ----------------------- #
-for i in range(NBRE_VILLE):
-    new_ville = pd.DataFrame({"Nom": ["Ville " + str(i)], "x": [rd.randrange(0, TAILLE_GRILLE_X)], "y": [rd.randrange(0, TAILLE_GRILLE_Y)]})
-    ville_df = pd.concat([ville_df, new_ville], ignore_index=True)
+# ---------------------------------------------------------------------------- #
+#                               Fonctions utiles                               #
+# ---------------------------------------------------------------------------- #
 
+# ----------------------- Creation ville aléatoirement ----------------------- #
+# ? Pour gerer si on donne un nom a la ville ou pas
+def add_city(nom = None):
+    if nom is None:
+        new_ville = pd.DataFrame({"Nom": "Ville " + str(len(ville_df)+1), "x": rd.randrange(0, TAILLE_GRILLE_X), "y": rd.randrange(0, TAILLE_GRILLE_Y)}, index = [0])
+    else:
+        new_ville = pd.DataFrame({"Nom": nom, "x": rd.randrange(0, TAILLE_GRILLE_X), "y": rd.randrange(0, TAILLE_GRILLE_Y)}, index = [0])
+    return new_ville
+    
 # ------------------------ Distance entre deux villes ------------------------ #
 def getDistance(nomVille1, nomVille2):
     x1 = ville_df.loc[ville_df["Nom"] == nomVille1, 'x'].values[0]
@@ -61,20 +67,65 @@ def getPopulation():
 
 # --------------------------- Creation des enfants --------------------------- #
 def getEnfants(parent1, parent2):
+    # *Recuperation ville venant du dataFrame
     liste_ville1 = parent1["Villes"]
     liste_ville2 = parent2["Villes"]
+    # ---------------------------------------------------------------------------- #
+    # *Creation d'un tableau contenant les villes
     liste_ville1 = liste_ville1.split(", ")
     liste_ville2 = liste_ville2.split(", ")
+    # ---------------------------------------------------------------------------- #
+    # *Creation de la premiere partie des enfants
     enfant1 = liste_ville1[:int(len(liste_ville1)/2)]
     enfant2 = liste_ville2[:int(len(liste_ville2)/2)]
+    # ---------------------------------------------------------------------------- #
+    # *Ajout de la seconde partie en partant de la fin de la liste des villes
     for i in range (len(liste_ville2)-1, -1, -1):
         if liste_ville2[i] not in enfant1:
             enfant1.append(liste_ville2[i])
     for i in range (len(liste_ville1)-1, -1, -1):
         if liste_ville1[i] not in enfant2:
             enfant2.append(liste_ville1[i])
+    # ---------------------------------------------------------------------------- #
+    # * Calcul Score des enfants
+    distances = []
+    for i in range(len(enfant1) - 1):
+        distance = getDistance(enfant1[i], enfant1[i+1])
+        distances.append(distance)
+    enfant1 = {"Villes" : ", ".join(enfant1), "Score" : sum(distances)}
+    distances = []
+    for i in range(len(enfant2) - 1):
+        distance = getDistance(enfant2[i], enfant2[i+1])
+        distances.append(distance)
+    enfant2 = {"Villes" : ", ".join(enfant2), "Score" : sum(distances)}
+    enfants_df = pd.DataFrame([enfant1, enfant2])
+    return enfants_df
+
+# --------------------------------- Mutation --------------------------------- #
+def mutate (individu):
+    liste_ville = individu["Villes"]
+    liste_ville = liste_ville.split(", ")
+    i = rd.randint(0, len(liste_ville))
+    j = rd.randint(0, len(liste_ville))
+    while (j == i):
+        j = rd.randint(0,len(liste_ville))
+    tempo = liste_ville[i]
+    liste_ville[i] = liste_ville[j]
+    liste_ville[j] = tempo
+    distances = []
+    for i in range(len(liste_ville) - 1):
+        distance = getDistance(liste_ville[i], liste_ville[i+1])
+        distances.append(distance)
+    new_individu = {"Villes" : ", ".join(liste_ville), "Score" : sum(distances)}
+    return pd.DataFrame([new_individu])
     
+# ---------------------------------------------------------------------------- #
+#                              Partie Utilisation                              #
+# ---------------------------------------------------------------------------- #
+cpt = 0
+while cpt < NBRE_VILLE:
+    city = add_city()
+    ville_df = pd.concat([ville_df, city], ignore_index=True)
+    cpt = cpt + 1
 
 pop = getPopulation()
-print(pop)
-getEnfants(pop.iloc[0], pop.iloc[1])
